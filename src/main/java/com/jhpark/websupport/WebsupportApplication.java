4d163184fad3ff01c20de3666d19871892b47760
Park-Ji-Hoon <ggoowlgns@naver.com>
2021-08-28 21:14:37 +0900

    Initial commit

---
+++ src/main/java/com/jhpark/websupport/WebsupportApplication.java
@@ -0,0 +1,13 @@
+package com.jhpark.websupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsupportApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebsupportApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jhpark/websupport/WebsupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhpark.websupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsupportApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
